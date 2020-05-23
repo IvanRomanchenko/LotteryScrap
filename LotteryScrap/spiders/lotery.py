ce41af0709f338c8c955da1bd2ffed505d79511d
Ivan Romanchenko <vanvanych789@gmail.com>
2020-05-23 13:25:52 +0300

    Add project files

---
+++ LotteryScrap/spiders/lotery.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+import scrapy
+
+
+class LoterySpider(scrapy.Spider):
+    name = 'lottery'
+    start_urls = ["https://igra.msl.ua/megalote/ru/archive/"]
+
+    def parse(self, response):
+        for i in response.xpath("//div[contains(@class, 'archive_results-item')]"):
+            yield {
+                "edition": i.xpath(".//a[contains(@class, 'archive_result-number')]/text()").re(r"\d+")[0],
+                "result-year": i.xpath(".//p[contains(@class, 'archive_result-date')]/text()").re(r"\d{4}")[0],
+                "created-time": datetime.utcnow().isoformat(timespec='seconds') + '+00:00',
+                "numbers": [int(j) for j in i.xpath(".//span[contains(@class, 'ball-number')]/text()").getall()]
+            }
+
+        pagination_links = response.css('li.next a')
+        yield from response.follow_all(pagination_links, self.parse)
+        
---
+++ LotteryScrap/spiders/oregon_lottery.py
@@ -0,0 +1,18 @@
+import scrapy
+from datetime import datetime, timezone, timedelta
+import pytz 
+
+
+class OregonLotterySpider(scrapy.Spider):
+    name = "oregon_lottery"
+    start_urls = ["https://www.oregonlottery.org/games/draw-games/keno/past-results/"]
+        
+    def parse(self, response):
+        for i in response.xpath("//table[contains(@class, 'responsive keno-table')]/tr")[1:]:
+            yield {
+                "externalId": f"oregonKeno_{i.xpath('.//td[2]/text()').get()}",
+                "drawDate": pytz.timezone("America/Los_Angeles").localize(datetime.strptime(i.xpath('.//td[1]/text()').get(), '%m/%d/%Y %H:%M%p')).astimezone(tz=timezone.utc).isoformat(),
+                "numbers": [int(j) for j in i.xpath('.//td/text()')[2:22].getall()],
+                "created": datetime.utcnow().isoformat(timespec='seconds') + '+00:00'
+                }
+            
