383997d1e561e5858478bb44e89bdd73d33d81e3
Ivan Romanchenko <vanvanych789@gmail.com>
2020-05-29 22:37:36 +0300

    Added FormRequest for AJAX-form for scraping without pagination.

--- LotteryScrap/spiders/oregon_lottery.py
+++ LotteryScrap/spiders/oregon_lottery.py
@@ -2,12 +2,22 @@ import scrapy
 from datetime import datetime, timezone, timedelta
 import pytz 
 
+from scrapy.http import FormRequest
+
 
 class OregonLotterySpider(scrapy.Spider):
     name = "oregon_lottery"
     start_urls = ["https://www.oregonlottery.org/games/draw-games/keno/past-results/"]
-        
+
     def parse(self, response):
+        return FormRequest.from_response(response, formxpath="//a[contains(@class, 'pagination-show-all')]", formdata={
+            'ctl22_TSSM': ';Telerik.Sitefinity.Resources, Version=10.2.6602.0, Culture=neutral, PublicKeyToken=b28c218413bdf563:en:1c3e6627-a90c-4375-b55c-75906376ec60:7a90d6a',
+            '__VIEWSTATE': '/wEPDwUKLTY1MzU2OTUwMmRkvA+XEUVx78gBN+PYgqp1AOoTZU2JKdB4oRoX9Q5fBVo=',
+            '__VIEWSTATEGENERATOR': '81854762',
+            'PageSize': '0'
+        }, method='POST', callback=self.after_parse)
+
+    def after_parse(self, response):
         for i in response.xpath("//table[contains(@class, 'responsive keno-table')]/tr")[1:]:
             yield {
                 "externalId": f"oregonKeno_{i.xpath('.//td[2]/text()').get()}",
